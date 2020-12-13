e8dd2245416a676665061e561dd44a23fd9cb8d8
prateek7861 <prateekgoyal7861@gmail.com>
2020-12-13 23:30:32 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Simple Banking Website</title>
+	<link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css">
+    <style>
+@import url('https://fonts.googleapis.com/css2?family=Lato:wght@400;900&display=swap');
+</style>
+</head>
+<body>
+<div class="topnav">
+  <a class="active" href="index.php">Home</a>
+  <div class="topnav-right">
+  <a class="active" href="transferdetails.php">Transfer History</a>
+  <a class="active" href="viewusers.php">View Users</a>
+
+  </div>
+</div>
+</div>  
+<h1 style="color:black;">Basic Banking System</h1>
+<div class="container">
+        <div class="button" >
+          <a class="text" href="viewusers.php">View Users</p>
+        </div>
+        <br><br><br>
+        <div class="button" >
+          <a class="text" href="transfer.php">Credit Transfer</a>
+        </div>
+  </div>
+  <div class="middle">
+     <h3>By Prateek Kumar Goyal</h3>
+     <a class="btn" href="">
+        <i class="fab fa-github"></i>
+     </a>
+     <a class="btn" href="">
+       <i class="fab fa-linkedin"></i>
+     </a>
+  </div>
+
+</body>
+</html>
